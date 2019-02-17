2272d5e58a1e4fb651044709d0329b3dc8ed91e9
Miguel Rojas <migueldrojascortes@icloud.com>
2019-02-16 21:09:11 -0500

    Adds the whole Knob framework

---
+++ KnobControl/KnobControl.h
@@ -0,0 +1,19 @@
+//
+//  KnobControl.h
+//  KnobControl
+//
+//  Created by Miguel Rojas on 2/16/19.
+//  Copyright © 2019 MRC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KnobControl.
+FOUNDATION_EXPORT double KnobControlVersionNumber;
+
+//! Project version string for KnobControl.
+FOUNDATION_EXPORT const unsigned char KnobControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KnobControl/PublicHeader.h>
+
+
